73fc87be1c5ee4af53bed818e113afba584c1581
Giovanni <giovanni_neves@tutanota.com>
2023-08-07 22:11:27 -0300

    Implementando Design e contexto

--- MetodosREST/Services/Implementations/PessoaImplementation.cs
+++ MetodosREST/Services/Implementations/PessoaImplementation.cs
@@ -1,9 +1,18 @@
 using MetodosREST.Models;
+using MetodosREST.Models.Context;
 
 namespace MetodosREST.Services.Implementations
 {
     public class PessoaServiceImplementation : IPessoaService
     {
+
+        private readonly MySQLContext _context;
+
+        public PessoaServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
         public Pessoa Create(Pessoa pessoa)
         {
             throw new NotImplementedException();
@@ -16,7 +25,7 @@ namespace MetodosREST.Services.Implementations
 
         public List<Pessoa> FindAll()
         {
-            throw new NotImplementedException();
+            return _context.Pessoas.ToList();
         }
 
         public Pessoa FindById(long id)
